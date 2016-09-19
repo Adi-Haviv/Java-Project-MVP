c01c60c37ddad792284913555148384afc99a04b
Adi-Haviv <adi.haviv@gmail.com>
2016-09-19 19:29:47 +0300

    S

---
+++ src/dbConnection/MazeSaveObject.java
@@ -0,0 +1,101 @@
+package dbConnection;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import algorithms.mazeGenerators.Maze3d;
+
+
+
+public class MazeSaveObject {
+	
+    public Maze3d javaObject=null;
+
+    public Maze3d getJavaObject() {
+        return javaObject;
+    }
+
+    public void setJavaObject(Maze3d javaObject) {
+        this.javaObject = javaObject;
+    }
+    
+    public  void saveObject(String name) throws Exception
+    {
+        try{
+        Connection conn= DriverManager.getConnection("jdbc:mysql://54.69.232.190:3306/damn-java", "root", "password");
+        PreparedStatement ps=null;
+        String sql=null;
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+
+        oos.writeObject(javaObject);
+        oos.flush();
+        oos.close();
+        bos.close();
+
+        byte[] data = bos.toByteArray();
+
+
+        sql="insert into mazes (name,maze) values (?,?)";
+        ps=conn.prepareStatement(sql);
+        ps.setObject(1, name);
+        ps.setObject(2, data);
+        ps.executeUpdate();
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public Maze3d getObject(String name) throws Exception
+    {
+        Connection conn = DriverManager.getConnection("jdbc:mysql://54.69.232.190:3306/damn-java", "root", "password");
+        PreparedStatement ps=null;
+        ResultSet rs=null;
+        String sql=null;
+
+        sql="select * from mazes where name LIKE '?'";
+
+        ps=conn.prepareStatement(sql);
+        ps.setObject(1, name);
+        rs=ps.executeQuery();
+
+        if(rs.next())
+        {
+            ByteArrayInputStream bais;
+
+            ObjectInputStream ins;
+
+            try {
+
+            bais = new ByteArrayInputStream(rs.getBytes("maze"));
+
+            ins = new ObjectInputStream(bais);
+
+            Maze3d maze = (Maze3d) ins.readObject();
+
+            ins.close();
+            
+            return maze;
+            }
+            catch (Exception e) {
+
+            e.printStackTrace();
+            }
+
+        }
+
+        return null;
+    }
+}
---
+++ src/dbConnection/SolutionSaveObject.java
@@ -0,0 +1,101 @@
+package dbConnection;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import algorithms.mazeGenerators.Position;
+import algorithms.search.Solution;
+
+
+public class SolutionSaveObject {
+	
+    public Solution<Position> javaObject=null;
+
+    public Solution<Position> getJavaObject() {
+        return javaObject;
+    }
+
+    public void setJavaObject(Solution<Position> javaObject) {
+        this.javaObject = javaObject;
+    }
+    
+    public  void saveObject(String name) throws Exception
+    {
+        try{
+        Connection conn= DriverManager.getConnection("jdbc:mysql://54.69.232.190:3306/damn-java", "root", "password");
+        PreparedStatement ps=null;
+        String sql=null;
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+
+        oos.writeObject(javaObject);
+        oos.flush();
+        oos.close();
+        bos.close();
+
+        byte[] data = bos.toByteArray();
+
+
+        sql="insert into solutions (name,solution) values (?,?)";
+        ps=conn.prepareStatement(sql);
+        ps.setObject(1, name);
+        ps.setObject(2, data);
+        ps.executeUpdate();
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public Solution<Position> getObject(String name) throws Exception
+    {
+        Connection conn = DriverManager.getConnection("jdbc:mysql://54.69.232.190:3306/damn-java", "root", "password");
+        PreparedStatement ps=null;
+        ResultSet rs=null;
+        String sql=null;
+
+        sql="select * from solutions where name LIKE '?'";
+
+        ps=conn.prepareStatement(sql);
+        ps.setObject(1, name);
+        rs=ps.executeQuery();
+
+        if(rs.next())
+        {
+            ByteArrayInputStream bais;
+
+            ObjectInputStream ins;
+
+            try {
+
+            bais = new ByteArrayInputStream(rs.getBytes("solution"));
+
+            ins = new ObjectInputStream(bais);
+
+            Solution<Position> sol = (Solution<Position>)ins.readObject();
+
+            ins.close();
+            
+            return sol;
+            }
+            catch (Exception e) {
+
+            e.printStackTrace();
+            }
+
+        }
+
+        return null;
+    }
+}
