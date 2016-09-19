7833aad8bf8d7f6033a6cc5d0e77ec6918f62428
Bar <Bar@Bars-pc>
2016-09-19 14:33:20 +0300

    tHE start of the start

---
+++ src/model/MyModel.java
@@ -0,0 +1,20 @@
+package model;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+public class MyModel implements Model, Observable {
+
+	@Override
+	public void addListener(InvalidationListener arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeListener(InvalidationListener arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/view/MyView.java
@@ -0,0 +1,20 @@
+package view;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+public class MyView implements Observable, View {
+
+	@Override
+	public void addListener(InvalidationListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeListener(InvalidationListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
